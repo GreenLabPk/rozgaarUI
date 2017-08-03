13dcad10c429370c882ad409646b2333fe80e8d1
Dares <m121h@live.com>
2017-08-02 20:38:47 -0800

    Add project files.

---
+++ rozgaarUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rozgaarUI.Startup))]
+namespace rozgaarUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
